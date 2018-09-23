5a6e114441432b8a55d213d69a449c22243ffb6b
Maurice Parker <mo@vincode.io>
2018-09-22 19:47:25 -0500

    set up to generate test files for VinContent regression test suite

--- VinContentViewer/ViewController.swift
+++ VinContentViewer/ViewController.swift
@@ -35,13 +35,44 @@ class ViewController: NSViewController {
                 webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
             ])
         
-        let url = URL(string: "https://9to5mac.com/2018/09/20/apple-miniseries-defending-jacob/")
+        guard let documentDirectory = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first else {
+            return
+        }
+        
+        
+//        let url = URL(string: "https://kottke.org/18/09/the-fish-copter-cactus-binky-and-other-clever-visual-mashups")
 //        let url = URL(string: "https://blog.chromium.org/2018/02/chrome-65-beta-css-paint-api-and.html")
 //        let url = URL(string: "https://developer.apple.com/documentation/uikit/view_controllers/preserving_your_app_s_ui_across_launches?language=swift")
-        let contentArticle = try! ContentExtractor.extractArticle(from: url!)
-        let html = renderHTML(withBody: contentArticle.wrappedContent!)
+        
+        let testName = "wired"
+        let testSource = "https://www.wired.com/2017/03/now-we-know-why-microsoft-bought-linkedin/"
+        
+        // Load the input HTML
+        let testURL = URL(string: testSource)!
+        let inputHTML = try! String(contentsOf: testURL)
+
+        // Write out the source HTML for testing
+        let inputURL = documentDirectory.appendingPathComponent("\(testName)-input.html")
+        try! inputHTML.write(to: inputURL, atomically: true, encoding: .utf16)
+
+        let article = try! ContentExtractor.extractArticle(from: inputHTML, source: testURL)
+        let content = article.wrappedContent!
+
+        // Render the content in the window
+        let html = renderHTML(withBody: content)
         webView!.loadHTMLString(html, baseURL: nil)
 
+        // Write the result out for tests
+        let resultURL = documentDirectory.appendingPathComponent("\(testName)-expected.html")
+        try! content.write(to: resultURL, atomically: true, encoding: .utf16)
+        
+        print("**************************************************")
+        print("* Article Title: \(String(describing: article.title))")
+        print("* Article Publisher: \(String(describing: article.publisher))")
+        print("* Article Publish Date: \(String(describing: article.publishDate?.description))")
+        print("* Article Byline: \(String(describing: article.byline))")
+        print("* Article Source: \(String(describing: article.source?.absoluteString))")
+        print("**************************************************")
     }
 
     override var representedObject: Any? {
@@ -58,7 +89,7 @@ class ViewController: NSViewController {
         let css = try! NSString(contentsOfFile: path, encoding: String.Encoding.utf8.rawValue)
         s += "<style>\n\(css)\n</style>"
 
-        s += "\n\n</head><body id='bodyId' onload='startup()' class=dark>\n\n"
+        s += "\n\n</head><body id='bodyId' class=dark>\n\n"
         s += body
         s += "\n\n</body></html>"
         
